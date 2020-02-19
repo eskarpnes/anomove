69cd7ef4c337ac063db8417dfbe2197b01250ea6
eskarpnes <erlendj.skarpnes@gmail.com>
2020-02-19 12:24:53 +0100

    animate with colors

--- visualisation/skeleton.py
+++ visualisation/skeleton.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-data_path = "/home/login/datasets/"
+data_path = ""
 dataframe = pd.read_csv(os.path.join(data_path, "CIMA_short.csv"))
 
 vectors = [
@@ -29,54 +29,38 @@ vectors = [
     ("left_knee", "left_ankle")
 ]
 
-fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 16)
-width = 960
-height = 540
+# 960 x 540
+# 1280 x 720
 
+width = 1280
+height = 720
 
-def draw_skeleton(row):
-    frame = Image.new("RGB", (width, height), color="gray")
-    draw = ImageDraw.Draw(frame)
+ORANGE = (51, 153, 255)
 
-    for i in range(1, len(row), 2):
-        radius = 3
-        point = (int(row[i] * width), int(row[i+1]*height))
-        draw.ellipse([point[0]-radius, point[1]-radius, point[0]+radius, point[1]+radius], fill="red")
+def random_color():
+    return (np.random.randint(256), np.random.randint(256), np.random.randint(256))
 
-    for vector in vectors:
-        start_x = int(row[vector[0] + "_x"] * width)
-        start_y = int(row[vector[0] + "_y"] * height)
-        end_x = int(row[vector[1] + "_x"] * width)
-        end_y = int(row[vector[1] + "_y"] * height)
-        draw.line([start_x, start_y, end_x, end_y], fill="red", width=3)
-
-
-    draw.text((10, height - 22), str(int(row["frame"])), font=fnt, fill="black")
-
-    savepath = os.path.join(data_path, "temp", str(int(row["frame"])) + ".png")
-    if not os.path.exists(os.path.join(data_path, "temp")):
-        os.makedirs(os.path.join(data_path, "temp"))
-
-    frame.save(savepath)
+def lenght_color(length):
+    green_val = length*3 if length < 85 else 255
+    red_val = 255 - length*3 if length < 85 else 0
+    return (0, green_val, red_val)
 
 def draw_skeleton_cv2(row):
-    frame = np.zeros((height, width, 3), np.uint8)
+    frame = np.full((height, width, 3), 220, np.uint8)
     # draw = ImageDraw.Draw(frame)
-
 
     for vector in vectors:
         start_x = int(row[vector[0] + "_x"] * width)
         start_y = int(row[vector[0] + "_y"] * height)
         end_x = int(row[vector[1] + "_x"] * width)
         end_y = int(row[vector[1] + "_y"] * height)
-        frame = cv2.line(frame, (start_x, start_y), (end_x, end_y), (255,0,0), 3)
+        length = np.sqrt((start_x-end_x)**2 + (start_y-end_y)**2)
+        frame = cv2.line(frame, (start_x, start_y), (end_x, end_y), lenght_color(length), 3)
 
     for i in range(1, len(row), 2):
         radius = 3
         point = (int(row[i] * width), int(row[i+1]*height))
-        frame = cv2.circle(frame, point, 3, (0,0,255))
-
-    # draw.text((10, height - 22), str(int(row["frame"])), font=fnt, fill="black")
+        frame = cv2.circle(frame, point, 3, ORANGE, 3)
 
     frame_string = str(int(row["frame"]))
 
@@ -85,12 +69,15 @@ def draw_skeleton_cv2(row):
 
     return frame
 
-out_path = os.path.join(data_path, "skeleton.avi")
+def animate(dataframe, video_name):
+    out_path = os.path.join(data_path, video_name + ".avi")
 
-out = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc(*'XVID'), 30.0, (width, height))
+    out = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc(*'XVID'), 30.0, (width, height))
 
-for _, row in tqdm(dataframe.iterrows()):
-    frame = draw_skeleton_cv2(row)
-    out.write(frame)
+    for _, row in tqdm(dataframe.iterrows()):
+        frame = draw_skeleton_cv2(row)
+        out.write(frame)
 
-out.release()
+    out.release()
+
+animate(dataframe, "skeleton")
