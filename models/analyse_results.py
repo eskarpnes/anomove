f7862f5140ebeb1d47e8d57f54f776906bb94723
Erlend Skarpnes <erlendj.skarpnes@gmail.com>
2020-03-02 15:33:09 +0100

    fixed path

--- models/analyse_results.py
+++ models/analyse_results.py
@@ -1,22 +1,20 @@
 import pandas as pd
-import numpy as np
-
+import os
 
 def get_data(filename):
-    DATA_PATH = "C://Users//haavalo//Desktop//Master//anomove//models//" + filename
-    return pd.read_csv(DATA_PATH)
+    return pd.read_csv(filename)
 
 
 def print_results(filename):
     df = get_data(filename)
-
+    df = df.drop(columns=["Unnamed: 0"])
     # Sort by sensitivity and print
     with pd.option_context('display.max_rows', None, "display.expand_frame_repr", False):
         print('\n Printing the results from file: ' + str(filename) + "\n")
 
         print("\n Sorted by sensitivity and then specificity \n")
-        print(df.sort_values(by=["sensitivity", "specificity"]))
+        print(df.sort_values(by=["sensitivity", "specificity"], ascending=False))
 
         # Sort by specificity and print
         print("\n Sorted by specificity and then sensitivity \n")
-        print(df.sort_values(by=["specificity", "sensitivity"]))
+        print(df.sort_values(by=["specificity", "sensitivity"], ascending=False))
