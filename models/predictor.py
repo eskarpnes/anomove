30c69a7e69c667016db0081466192f4516cc2f2b
Erlend Skarpnes <erlend@skarpn.es>
2020-03-28 11:31:52 +0100

    predictions with thresholds

--- models/predictor.py
+++ models/predictor.py
@@ -15,7 +15,7 @@ class Predictor:
         self.model = None
 
     def load_model(self, model_name):
-        path = os.path.join("models/saved_models", model_name + ".joblib")
+        path = os.path.join("models", "saved_models", model_name + ".joblib")
         self.model = joblib.load(path)
 
     def save_model(self, model_name):
@@ -27,7 +27,14 @@ class Predictor:
     def predict(self, data_path, infant_id):
         print(f"Predicting infant {infant_id} - {strftime('%H:%M:%S', gmtime())}")
         window_sizes = [128, 256, 512, 1024]
-        etl = ETL(data_path, window_sizes)
+        etl = ETL(
+            data_path,
+            window_sizes,
+            pooling="mean",
+            sma_window=3,
+            bandwidth=5,
+            minimal_movement=0.1
+        )
         etl.load_infant(infant_id)
         print(f"Preprocessing the data - {strftime('%H:%M:%S', gmtime())}")
         etl.preprocess_pooled()
@@ -41,37 +48,67 @@ class Predictor:
         print(f"Generating fourier data - {strftime('%H:%M:%S', gmtime())}")
         for window_size in window_sizes:
             for angle in angles:
-                dataframe = etl.generate_fourier_data(window_size, angle, window_size)
+                dataframe = etl.generate_fourier_data(window_size, angle, window_size//8)
                 data_features = pd.DataFrame(dataframe.data.tolist())
-                data_transformed = self.model[window_size]["scaler"].transform(data_features)
-                data_transformed = self.model[window_size]["pca"].transform(data_transformed)
-                dataframe["label"] = self.model[window_size]["model"].predict(data_transformed)
+                if not data_features.empty:
+                    data_transformed = self.model[window_size]["scaler"].transform(data_features)
+                    data_transformed = self.model[window_size]["pca"].transform(data_transformed)
+                    dataframe["label"] = self.model[window_size]["model"].predict_proba(data_transformed)
+                else:
+                    dataframe["label"] = pd.Series([])
                 for _, row in dataframe.iterrows():
                     start = int(row["frame_start"])
                     for i in range(start, start+window_size):
-                        predictions[angle][i].append(int(row["label"]))
+                        predictions[angle][i].append(row["label"])
 
         print(f"Voting the results for each angle and frame...")
 
+
+
         for key, val in predictions.items():
+            thresholds = {
+                0.1: 0,
+                0.2: 0,
+                0.3: 0,
+                0.4: 0,
+                0.5: 0,
+                0.6: 0,
+                0.7: 0,
+                0.8: 0,
+                0.9: 0,
+                0.95: 0
+            }
             for i, frame in enumerate(predictions[key]):
                 if not frame:
-                    label = -1
-                elif 1 in frame:
-                    label = 1
-                else:
                     label = 0
+                    # continue
+                else:
+                    label = np.mean(frame)
+                    for threshold in thresholds.keys():
+                        if label > threshold:
+                            thresholds[threshold] += 1
                 predictions[key][i] = label
-
+            for threshold, val in thresholds.items():
+                try:
+                    thresholds[threshold] = val/len(predictions[key][predictions[key] != 0])
+                except ZeroDivisionError:
+                    thresholds[threshold] = 0
+            print(f"{key} - {np.mean(predictions[key][predictions[key] != 0])}")
+            print(f"{key} - {thresholds}")
         print(f"Done - {strftime('%H:%M:%S', gmtime())}")
-
+        print(f"Final results: {np.mean([np.mean(predictions[key]) for key in predictions.keys()])}")
         infant = etl.cima[infant_id]
 
         infant["predictions"] = pd.DataFrame(predictions)
 
         return infant
 
+
 if __name__ == "__main__":
     predictor = Predictor()
-    predictor.load_model("test_model")
-    predictor.predict("/home/login/datasets/CIMA/data", "001")
+    predictor.load_model("ensemble_model")
+    # 014 is impaired, 001 is healthy
+    infants = ["001", "002", "003", "014", "016", "019"]
+    for infant in infants:
+        print("-------------------------------------------------------------------------------------------------------")
+        predictor.predict("/home/erlend/datasets/CIMA/data", infant)
