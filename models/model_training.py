7d4430f47f999a28855919a806e97cd31aba10d3
Erlend Skarpnes <erlend@skarpn.es>
2020-03-28 11:31:08 +0100

    train ensemble

--- models/model_training.py
+++ models/model_training.py
@@ -2,6 +2,10 @@ import pandas as pd
 import joblib
 from sklearn import model_selection, neighbors, metrics
 from pyod.models.lof import LOF
+from pyod.models.abod import ABOD
+from pyod.models.knn import KNN
+from pyod.models.ocsvm import OCSVM
+from combo.models.detector_comb import SimpleDetectorAggregator
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import sys
@@ -10,14 +14,41 @@ import os
 sys.path.append("../")
 from etl.etl import ETL
 
-DATA_PATH = "/home/login/datasets"
+DATA_PATH = "/home/erlend/datasets"
 
 
 def load_data(dataset):
-    etl = ETL(DATA_PATH, [128, 256, 512, 1024], size=100)
+    etl = ETL(
+            DATA_PATH,
+            [128, 256, 512, 1024],
+            pooling="mean",
+            sma_window=3,
+            bandwidth=5,
+            minimal_movement=0.05
+        )
     etl.load(dataset)
     etl.preprocess_pooled()
-    etl.generate_fourier_dataset()
+    etl.generate_fourier_dataset(window_overlap=8)
+
+
+def construct_model():
+    classifiers = [
+        LOF(n_neighbors=1),
+        LOF(n_neighbors=3),
+        LOF(n_neighbors=5),
+        LOF(n_neighbors=7),
+        LOF(n_neighbors=8),
+        LOF(n_neighbors=9),
+        LOF(n_neighbors=10),
+        ABOD(n_neighbors=5),
+        KNN(n_neighbors=5),
+        OCSVM()
+    ]
+    model = SimpleDetectorAggregator(
+        classifiers,
+        method="average"
+    )
+    return model
 
 
 def train_model(model_name):
@@ -38,7 +69,7 @@ def train_model(model_name):
         X_pca = pca.fit_transform(X_scaled)
         # y = X["label"]
 
-        model = LOF(n_neighbors=10)
+        model = construct_model()
         model.fit(X_pca)
 
         models[window_size] = {
@@ -47,10 +78,12 @@ def train_model(model_name):
             "model": model
         }
 
+    if not os.path.exists("saved_models"):
+        os.mkdir("saved_models")
     save_path = os.path.join("saved_models", model_name + ".joblib")
     joblib.dump(models, save_path)
 
 
 if __name__ == "__main__":
     load_data("CIMA")
-    train_model("test_model")
+    train_model("ensemble_model_low_movement")
